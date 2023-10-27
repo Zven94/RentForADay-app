1ead97f6456f2fe2e86650b3994023a8069b53e2
Zven94 <nicobolanez@gmail.com>
2023-10-27 18:44:06 -0300

    fix linters issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception 
+  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
   def configure_devise_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user|
-        user.permit(:name, :email, :password, :password_confirmation)
+      user.permit(:name, :email, :password, :password_confirmation)
     end
-  end  
+  end
 end
