22cc68975a3f8914eb93d36ff461476f76a3242f
Cesar Herrera <cesarherr321@gmail.com>
2023-10-27 18:16:12 -0300

    Add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :create, :destroy] do
+        resources :appointments, only: [:index, :create, :destroy]
+      end
+      resources :items, only: [:index, :create, :destroy]
+    end
+  end
 end
