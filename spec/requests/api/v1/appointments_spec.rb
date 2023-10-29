6e5a76c0395c1519213d9453ad91d4b7221cb270
Cesar Herrera <cesarherr321@gmail.com>
2023-10-28 22:40:00 -0300

    Add "post" test for Appointment

--- spec/requests/api/v1/appointments_spec.rb
+++ spec/requests/api/v1/appointments_spec.rb
@@ -29,4 +29,39 @@ RSpec.describe 'Appointment API', type: :request do
       end
     end
   end
+
+  path '/api/v1/users/{user_id}/appointments' do
+    parameter name: :user_id, in: :path, type: :integer, description: 'user_id', required: true
+
+    post 'Add an appointment' do
+      tags 'Appointment'
+      consumes 'application/json', 'application/xml'
+      parameter name: :appointment, in: :body, description: 'New Apponitment',
+
+                schema: {
+                  type: :object,
+                  properties: {
+                    item_id: { type: :integer },
+                    city: { type: :string },
+                    date: { type: :date }
+                  }
+                }
+      response '201', 'blog created' do
+        let(:user_id) { User.create(name: 'Test name', email: 'test4@example.com', password: '123456').id }
+        let(:item_id) { Item.create(name: 'Item2', city: 'City', price: '100', description: 'Test description').id }
+        let(:appointment) { { item_id:, city: 'Test city', date: '02/10/2023' } }
+
+        run_test!
+      end
+
+      response '422', 'unprocessable_entity' do
+        let(:user_id) { User.create(name: 'Test name', email: 'test4@example.com', password: '123456').id }
+        let(:item_id) { Item.create(name: 'Item2', city: 'City', price: '100', description: 'Test description').id }
+        let(:appointment) { { item_id:, city: '', date: '02/10/2023' } }
+
+        run_test!
+      end
+    end
+  end
+
 end
