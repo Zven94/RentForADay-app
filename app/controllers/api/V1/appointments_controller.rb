a5afd5c658f406249399be4d806d37f4446e2148
Cesar Herrera <cesarherr321@gmail.com>
2023-10-28 22:34:41 -0300

    Refactor appointment model and controller

--- app/controllers/api/V1/appointments_controller.rb
+++ app/controllers/api/V1/appointments_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::AppointmentsController < ApplicationController
 
     render json: @appointments, status: 200
   rescue ActiveRecord::RecordNotFound
-    render json: { error: 'No reserves yet' }, status: :not_found
+    render json: { error: 'No reserves yet' }, status: 404
   end
 
   def create
@@ -15,18 +15,20 @@ class Api::V1::AppointmentsController < ApplicationController
     city = json_request['city']
     date = json_request['date']
 
-    @appointments = Appointment.create(user_id: params[:user_id], item_id: params[:item_id], date:, city:)
+    @appointment = Appointment.new(user_id: params[:user_id], item_id: params[:item_id], date:, city:)
 
-    render json: @appointments, status: 200
-  rescue ActiveRecord::RecordNotFound
-    render json: { error: 'No reserves yet' }, status: :not_found
+    if @appointment.save
+      render json: @appointment, status: :created
+    else
+      render json: { error: @appointment.errors.full_messages.to_sentence }, status: :unprocessable_entity
+    end
   end
 
   def destroy
     @appointments = Appointment.find(params[:id])
     @appointments.destroy
 
-    render json: { message: 'Appointment deleted' }, status: 200
+    render json: { message: 'Appointment deleted' }, status: 201
   rescue ActiveRecord::RecordNotFound
     render json: { error: 'Appointment not found' }, status: :not_found
   end
