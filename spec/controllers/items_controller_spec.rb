1b9862a1aa3673654145522236ecfd4c2096b7a2
Nicolas Bolañez <Zven94>
2023-10-29 15:05:40 -0300

    create test for items controller of the API

---
+++ spec/controllers/items_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::ItemsController, type: :controller do
+
+  describe "GET #index" do
+
+    before(:each) do
+      @item = Item.create(name: "Hause 1", price: 100, city: "Buenos Aires", description: "Hause 1 description")
+    end
+
+    it "returns http success: 200 code" do
+      get :index
+      expect(response).to have_http_status(200)
+    end
+
+    it "JSON body response contains expected item attributes" do
+      get :index
+      json_response = JSON.parse(response.body)
+      expect(json_response.first.keys).to match_array(["id", "name", "price", "city", "description", "image", "created_at", "updated_at"])
+    end
+
+    it "Show all items" do
+      get :index
+      json_response = JSON.parse(response.body)
+      expect(json_response.count).to eq(1)
+    end
+
+    it "Delete item" do
+      expect{ delete :destroy, params: { id: @item.id } }.to change(Item, :count).by(-1)
+    end
+  end
+end
