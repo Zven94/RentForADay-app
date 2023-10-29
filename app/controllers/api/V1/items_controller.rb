1b9c9ca2f37226fa9b6629a4fc9d3260dad34342
Nicolas Bolañez <Zven94>
2023-10-29 02:20:26 -0300

    added method for API to Item_controller

--- app/controllers/api/V1/items_controller.rb
+++ app/controllers/api/V1/items_controller.rb
@@ -1,2 +1,30 @@
 class Api::V1::ItemsController < ApplicationController
+  def index
+    render json: Item.all
+  end
+  def new
+    item = Item.new
+  end
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render json: item
+    else
+      render json: { errors: item.errors }, status: :unprocessable_entity
+    end
+  end
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    render json: item
+  end
+
+  private
+    def item_params 
+      params.require(:item).permit(:name, :description, :price)
+    end
 end
