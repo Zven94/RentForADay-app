1110ba648f7c855ba001531578629348354482f4
Ruthmy <ruthmy17@gmail.com>
2023-10-31 08:54:40 -0600

    Modified 'cors.rb' -file- - Implemented a different structure.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,13 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*' # later change to the domain of the frontend app
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put],
-    expose: [:Authorization]
+    # resource '*', headers: :any, methods: [:get, :post, :patch, :put],
+    # expose: [:Authorization]
+    resource(
+     '*',
+     headers: :any,
+     expose: ["Authorization"],
+     methods: [:get, :patch, :put, :delete, :post]
+    )
   end
 end
