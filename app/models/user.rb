e5acdb87213d95d8da7a8b94a88737195bb0ca77
Ruthmy <ruthmy17@gmail.com>
2023-10-31 09:19:31 -0600

    Modified 'user.rb' -model- - Added the jwt_authenticatable module

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :jwt_authenticatable, :recoverable, :rememberable, :validatable
   has_many :appointments
 
   validates :name, presence: true, length: { maximum: 50 }
