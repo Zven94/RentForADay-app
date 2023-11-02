a2edb8cc50941e4768be9cea4ecab5432d0c38f8
Ruthmy <ruthmy17@gmail.com>
2023-11-02 17:02:46 -0600

    Modified 'user.rb' -model- - Added revocation strategy to the model and configure it accordingly.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
   devise :database_authenticatable, :registerable,
-         :jwt_authenticatable, :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
   has_many :appointments
 
   validates :name, presence: true, length: { maximum: 50 }
